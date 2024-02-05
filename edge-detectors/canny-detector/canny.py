03d9dd9893a1c88ccaee60d0a7906c41761a99c0
thabsheerjm <tmachingal>
2024-02-04 23:37:28 -0500

    noise removal

--- edge-detectors/canny-detector/canny.py
+++ edge-detectors/canny-detector/canny.py
@@ -4,7 +4,17 @@ import numpy as np
 import cv2
 
 class canny:
-    pass
+    def __init__ (self):
+        pass
+
+    def gaussian_kernel(self,size,sigma=1.0):
+        size = int(size)//2
+        x,y = np.mgrid[-size:size+1,-size:size+1]
+        normal = 1/(2.0 *np.pi *sigma**2)
+        g= np.exp(-((x**2 + y**2)/(2.0*sigma**2)))* normal
+
+        return g
+    
 if __name__ == '__main__':
     img = cv2.imread('edge-detectors/images/1.jpg')
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
